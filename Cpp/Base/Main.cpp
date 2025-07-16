bd264bd54cf4faf467f3e6457a90d020600f8e75
김주영 <98079486+Leusin@users.noreply.github.com>
2025-07-16 12:27:42 +0900

    chore: 임시 주석 해제

--- Cpp/Base/Main.cpp
+++ Cpp/Base/Main.cpp
@@ -8,23 +8,21 @@ int main()
 {
 	Problem_4();
 
-	//Log("Hello World");
+	Log("Hello World");
 
-	//// cout - console out 이라는 뜻
-	//// << shift 연산자를 오버라이딩한 것
-	//printf("Hello World %d %s\n", 10, "string test\n"); // C style
-	//std::cout << "Hello  W " << 10 << " " << "string test\n"; // C++ style
-	//
-	//int val1 = 0;
-	//int val2 = 0;
+	// cout - console out 이라는 뜻
+	// << shift 연산자를 오버라이딩한 것
+	printf("Hello World %d %s\n", 10, "string test\n"); // C style
+	std::cout << "Hello  W " << 10 << " " << "string test\n"; // C++ style
+	
+	int val1 = 0;
+	int val2 = 0;
 
-	//// 입출력
-	//std::cin >> val1;
-	//std::cout << val1;
+	// 입출력
+	std::cin >> val1;
+	std::cout << val1;
 
-	//std::cin.get(); // 출력 후 종료되지 않도록 함
-
-
+	std::cin.get(); // 출력 후 종료되지 않도록 함
 
 	return 0;
 }
