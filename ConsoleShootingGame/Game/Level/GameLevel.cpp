ab2a2fdfb4b1282f8ac9e224669c28cd3d159439
Leusin <leusin@naver.com>
2025-07-31 12:01:09 +0900

    work: 슈팅 게임 GameLevel 에서 적 생성 로직 구현
    - GameLevel 클래스에서 적 이미지 종류 배열 추가
    - 랜덤한 시간에 스폰하도록 구현
    - main 함수에 메모리 누수 검사 코드 추가

--- ConsoleShootingGame/Game/Level/GameLevel.cpp
+++ ConsoleShootingGame/Game/Level/GameLevel.cpp
@@ -1,14 +1,25 @@
 #include "GameLevel.h"
 
+#include "Utils/Utils.h"
 #include "Actor/Player.h"
 #include "Actor/Enemy.h"
 
 
+// 적 생성할 때 사용할 글자 값.
+const char* GameLevel::enemyType[] =
+{
+	";:^:;",
+	"zZwZz",
+	"oO@Oo",
+	"<-=->",
+	")qOp(",
+};
+
 GameLevel::GameLevel()
 {
 	AddActor(new Player()); // 플레이어 추가
-	
-	AddActor(new Enemy());
+
+	enemySpawnTimer.SetTargetTime(Utils::RandomFloat(2.0f, 3.0f));
 }
 
 GameLevel::~GameLevel()
@@ -23,6 +34,29 @@ void GameLevel::BeginPlay()
 void GameLevel::Tick(float deltaTime)
 {
 	super::Tick(deltaTime);
+
+	enemySpawnTimer.Tick(deltaTime);
+
+	if (!enemySpawnTimer.IsTimeout()) // 타이머 확인
+	{
+		return;
+	}
+
+	// 적 생성 로직
+	{
+		// 배열 길이 구하기
+		static int enemyTypeCount = sizeof(enemyType) / sizeof(enemyType[0]);
+		// 적 종류
+		int index = Utils::Random(0, enemyTypeCount - 1);
+		// y 위치 랜덤으로
+		int yPosition = Utils::Random(1, 10);
+		// 적생성 
+		AddActor(new Enemy(enemyType[index], yPosition));
+	}
+
+	// 타이머 정리
+	enemySpawnTimer.Reset();
+	enemySpawnTimer.SetTargetTime(Utils::RandomFloat(2.0f, 3.0f));
 }
 
 void GameLevel::Render()
--- ConsoleShootingGame/Game/Level/GameLevel.h
+++ ConsoleShootingGame/Game/Level/GameLevel.h
@@ -1,7 +1,3 @@
-#pragma once
-
-#include "Level/Level.h"
-
 /*
 * TODO: GameLevel 구현할 것
 * [] npc 생성: 랜덤성을 가미해서 시간마다 적 생성
@@ -10,6 +6,12 @@
 * [] 게임 판정: 플레이어 사망
 * [] 게임 목표: ???
 */
+
+#pragma once
+
+#include "Level/Level.h"
+
+#include "Utils/Timer.h"
 
 /// <summary>
 /// 슈팅 게임 메인 레벨
@@ -25,4 +27,10 @@ public:
 	virtual void BeginPlay() override;
 	virtual void Tick(float deltaTime) override;
 	virtual void Render() override;
+
+private:
+	Timer enemySpawnTimer;
+
+	// 적 생성할 때 사용할 글자 값
+	static const char* enemyType[];
 };
